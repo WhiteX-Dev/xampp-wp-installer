f1aa782b1e7b1bdca38cd89eb155a3292b5f1f61
WhiteX <arpad.szucs@gmail.com>
2022-05-03 23:47:15 +0300

    Changed config file

--- config-sample.php
+++ config-sample.php
@@ -1,11 +1,27 @@
 <?php
 
-// MySQL config file (local file ignored by Git)
+// Install config file (local file ignored by Git)
 // Duplicate this file and rename it to "config.php"
 
 $servername = "localhost"; // MySQL server name
 $username = "root"; // Change this to your username
 $password = ""; // Change this to your password
 
+// WP admin email
+$admin_email = "admin@whitex.cloud"; // Change this to your email	
+
 // XAMPP path
 $xampp_path = "C:/xampp/htdocs/"; // Change this to your XAMPP path
+
+
+// For later - You can disregard the following lines for now ======================== //
+
+//WP-CLI path
+$wpcli_path = "C:/xampp/php/wp-cli.phar"; // Change this to your WP-CLI path
+
+// PHP paths
+$php_path = "C:/xampp/php/php.exe"; // Change this to your PHP path
+$php_ini_path = "C:/xampp/php/php.ini"; // Change this to your PHP ini path
+
+// MySQL path
+$mysql_path = "C:/xampp/mysql/bin/mysql.exe"; // Change this to your MySQL path
